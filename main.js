096f12f3f06b53c24d7023279a51700b88a0823b
leifehetland <lehetland1@gmail.com>
2023-08-29 22:41:36 -0400

    added main.ts that transpiles to main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+var a = "1";
+// Run tsc name_of_file
+// Example of error
+// because myfunc is not a method on a
+console.log("test", a.myfunc());
+// Error is thrown when trying to transpile
+// Run tsc name_of_file -w 
+// to run in watch mode
